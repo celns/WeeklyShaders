976593c0ea863019cc62cf0dca6ccf9dbcb84df0
celns <celns@outlook.com>
2019-11-30 23:28:41 +0800

    使用纹理
    Signed-off-by: celns <celns@outlook.com>

--- ImageProcessing/Texture.glsl
+++ ImageProcessing/Texture.glsl
@@ -5,9 +5,14 @@
 #ifdef GL_ES
 precision mediump float;
 #endif
+//对于wrap模式
+//使用等比例图片 会自动repeat模式
+//使用非等比图片，会使用border模式
+uniform sampler2D u_texture_0;
+uniform sampler2D u_tex0Resolution;
 
-uniform sampler2D u_tex0;
-uniform vec2 u_tex0Resolution;
+uniform sampler2D u_texture_1;
+uniform vec2 u_tex1Resolution;
 
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
@@ -18,7 +23,7 @@ void main () {
     st.x *= u_resolution.x / u_resolution.y;
     vec4 color = vec4(st.x,st.y,0.0,1.0);
 
-    color = texture2D(u_tex0,st);
+    color = texture2D(u_texture_0,st);
 
     gl_FragColor = color;
 }
