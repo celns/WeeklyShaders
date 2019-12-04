d07199d0440ee9358a5ad6caf4705da65626d295
celns <celns@outlook.com>
2019-12-04 15:53:58 +0800

    万花镜
    Signed-off-by: celns <celns@outlook.com>

--- ImageProcessing/Texturekaleidoscope.glsl
+++ ImageProcessing/Texturekaleidoscope.glsl
@@ -1,4 +1,15 @@
 //万花镜
+//整个变换并不是移动了texture，而是变换了像素坐标st，或者可以称之为uv
+
+//极坐标与笛卡尔坐标的互相转换
+//笛卡尔转极坐标
+//tan θ = y/x,
+// 所以 θ = atan（y/x)
+//r = sqrt(x*x + y*y)
+//极坐标转笛卡尔
+//x = r * cos(θ)
+//y = r * sin(θ)
+
 
 #ifdef GL_ES
 precision mediump float;
@@ -8,12 +19,13 @@ precision mediump float;
 #define TWO_PI 6.28318530718
 
 uniform sampler2D u_texture_0;
-uniform vec2 u_tex0Resolution;
+//uniform vec2 u_tex0Resolution;
 
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
-float xoffset = 1.5;
+//x轴偏移
+float xoffset = 1.0;
 
 // Based on Asalga shader
 // https://www.shadertoy.com/view/4ss3WX
@@ -21,25 +33,50 @@ void main () {
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
     st.x *= u_resolution.x /u_resolution.y;
     st.x -= xoffset;
-    float time = u_time*0.2;
+    float time = u_time*0.5;
 
-    // move to the center
+    // 中心偏移
     st = st - 0.5;
 
-    // cartesian to polar coordinates
-    float r = length(st);
-    float a = atan(st.y, st.x);
+    // 笛卡尔转换为极坐标
+    float radius = length(st);
 
-    // Repeat side acoriding to angle
-    float sides = 10.;
-    float ma = mod(a, TWO_PI/sides);
-    ma = abs(ma - PI/sides);
+    //下面两个atan是相同的, 
+    //但是用第二个应该会有性能提升(这里需要取查看atan的源码来下定论)
+    //float theta = atan( st.y/st.x );
+    float theta = atan( st.y, st.x);
 
-    // polar to cartesian coordinates
-    st = r * vec2(cos(ma), sin(ma));
-    st = fract(st+time);
+
+    // 万花个数
+    //也就是基本图元的重复个数
+    float side_num = 20.0;
+    //
+    //万花镜的移动是向着极坐标中心，并且图元总是中心对称的
+    //
+    //移动角度
+    //theta 对 2Π/side_num 取余
+    float ma = mod(theta, TWO_PI/side_num);
+    //切分，就是说将每个side进行内部切分，对半切，由分形的元素
+    ma = abs(ma - PI/side_num);
+    //
+    //上面，用TWO_PI进行图元复制
+    //用PI进行切分
+    //用成2倍的数字都可以完成这项工作
+    //
+
+    // 极坐标转到笛卡尔坐标，使用ma作为角度进行变换
+    //对radius进行的移动处理
+    st = radius * vec2(cos(ma), sin(ma));
+
+    //试一试同心圆
+    //st = radius * vec2(2.0);
+
+    //动起来，试试正负号不同的效果
+    st = fract(st - time);
+    //st = fract(st + time);
 
     vec4 color = vec4(st.x,st.y,0.0,1.0);
+    //vec4 color = vec4(0.0);
     color = texture2D(u_texture_0,st);
 
     gl_FragColor = color;
