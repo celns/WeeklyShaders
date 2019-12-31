dcbd655004ddd821b15091486385a63ac388ed8e
celns <celns@outlook.com>
2019-12-31 15:57:59 +0800

    starnest
    Signed-off-by: celns <celns@outlook.com>

--- ShaderToy/StarNest.glsl
+++ ShaderToy/StarNest.glsl
@@ -3,13 +3,13 @@
 
 // This content is under the MIT License.
 
-#define iterations 17
+#define iterations 20
 #define formuparam 0.53
 
-#define volsteps 20
+#define volsteps 10
 #define stepsize 0.1
 
-#define zoom   0.800
+#define zoom   1.5
 #define tile   0.850
 #define speed  0.010 
 
@@ -24,7 +24,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	//get coords and direction
 	vec2 uv=fragCoord.xy/iResolution.xy-.5;
 	uv.y*=iResolution.y/iResolution.x;
-	vec3 dir=vec3(uv*zoom,1.);
+
+	vec3 dir=vec3(uv*zoom,1.0);//z轴方向
+	
 	float time=iTime*speed+.25;
 
 	//mouse rotation
@@ -34,19 +36,24 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	mat2 rot2=mat2(cos(a2),sin(a2),-sin(a2),cos(a2));
 	dir.xz*=rot1;
 	dir.xy*=rot2;
-	vec3 from=vec3(1.,.5,0.5);
+
+	vec3 from=vec3(1.0,0.5,0.5);
 	from+=vec3(time*2.,time,-2.);
 	from.xz*=rot1;
 	from.xy*=rot2;
 	
 	//volumetric rendering
-	float s=0.1,fade=1.;
-	vec3 v=vec3(0.);
-	for (int r=0; r<volsteps; r++) {
+	//s: bright
+	float s=0.1,fade=1.0;
+	vec3 color=vec3(0.0);
+	for (int r=0; r<volsteps; r++) //体积步数
+	{
 		vec3 p=from+s*dir*.5;
+		//p = s * dir * 0.5;
 		p = abs(vec3(tile)-mod(p,vec3(tile*2.))); // tiling fold
-		float pa,a=pa=0.;
-		for (int i=0; i<iterations; i++) { 
+		float pa,a=0.0;
+		for (int i=0; i<iterations; i++) 
+		{ 
 			p=abs(p)/dot(p,p)-formuparam; // the magic formula
 			a+=abs(length(p)-pa); // absolute sum of average change
 			pa=length(p);
@@ -55,12 +62,14 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 		a*=a*a; // add contrast
 		if (r>6) fade*=1.-dm; // dark matter, don't render near
 		//v+=vec3(dm,dm*.5,0.);
-		v+=fade;
-		v+=vec3(s,s*s,s*s*s*s)*a*brightness*fade; // coloring based on distance
+		color+=fade;
+		color+=vec3(s,s*s,s*s*s*s)*a*brightness*fade; // coloring based on distance
+
+		//是一种fbm的变体
 		fade*=distfading; // distance fading
 		s+=stepsize;
 	}
-	v=mix(vec3(length(v)),v,saturation); //color adjust
-	fragColor = vec4(v*.01,1.);	
+	color=mix(vec3(length(color)),color,saturation); //color adjust
+	fragColor = vec4(color*0.01,1.0);	
 	
 }
